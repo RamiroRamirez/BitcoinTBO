8cef80d59be32bb35bde801df86b5a8974f3d681
Ramiro Ramírez <ramiro.ramirez07@gmail.com>
2019-11-19 18:50:44 +0100

    Added APIManager to handle requests

---
+++ BitcoinTBO/Constants.swift
@@ -0,0 +1,16 @@
+//
+//  Constants.swift
+//  BitcoinTBO
+//
+//  Created by Ramiro Ramirez on 19.11.19.
+//  Copyright © 2019 Ramiro Ramirez. All rights reserved.
+//
+
+import Foundation
+
+struct Constants {
+    
+    struct API {
+        
+    }
+}
