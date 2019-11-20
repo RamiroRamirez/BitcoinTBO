83b91c541050e7dde69a5f8802793c58dd8b798d
Ramiro Ramirez <ramiro.ramirez@smartmobilefactory.com>
2019-11-20 17:14:33 +0100

    Fixed indentation

--- BitcoinTBO/Scenes/BitcoinEuro/Managers/BitcoinInformationManager.swift
+++ BitcoinTBO/Scenes/BitcoinEuro/Managers/BitcoinInformationManager.swift
@@ -12,29 +12,29 @@ struct BitcoinInformationManager {
 	
 	/// Method to fetch bitcoin information
 	/// - Parameter completion: completion containing array of BitcoinDayInformation objects and error when needed
-    static func fetchBitcoinInformation(completion: ((_ bitcoinDayInformations: [BitcoinDayInformation], _ error: Error?) -> Void)?) {
+	static func fetchBitcoinInformation(completion: ((_ bitcoinDayInformations: [BitcoinDayInformation], _ error: Error?) -> Void)?) {
 		guard let url = Constants.API.baseUrlV2?.add(path: Constants.API.Endpoint.historicalDaily.rawValue) else {
 			completion?([], APIManager.Invalid.format.localizedError)
-            return
-        }
+			return
+		}
 
 		let parameters = [Constants.API.Keys.fsym.rawValue: Constants.API.Values.bitcoin.rawValue,
 						  Constants.API.Keys.tsym.rawValue: Constants.API.Values.euro.rawValue]
 		let requestElements = APIManager.RequestElements(url: url, parameters: parameters, headers: nil)
-        
-		APIManager.get(requestElements: requestElements, success: { (responseData: Any?) in
-            guard let data = responseData as? Data else {
-				completion?([], APIManager.Invalid.format.localizedError)
-                return
-            }
-            
-            let bitcoinInformation = self.bitcoinInformation(from: data)
-            completion?(bitcoinInformation.bitcoinDayInformations, bitcoinInformation.error)
 
-        }, failure: { (error: Error) in
-            completion?([], error)
-        })
-    }
+		APIManager.get(requestElements: requestElements, success: { (responseData: Any?) in
+			guard let data = responseData as? Data else {
+				completion?([], APIManager.Invalid.format.localizedError)
+				return
+			}
+
+			let bitcoinInformation = self.bitcoinInformation(from: data)
+			completion?(bitcoinInformation.bitcoinDayInformations, bitcoinInformation.error)
+
+		}, failure: { (error: Error) in
+			completion?([], error)
+		})
+	}
 	
 	static func fetchCurrentBitcoinInformation(completion: ((_ bitcoin: Bitcoin?, _ error: Error?) -> Void)?) {
 		guard let url = Constants.API.baseUrl?.add(path: Constants.API.Endpoint.price.rawValue) else {
@@ -49,8 +49,8 @@ struct BitcoinInformationManager {
 		APIManager.get(requestElements: requestElements, success: { (responseData: Any?) in
 			guard let data = responseData as? Data else {
 				completion?(nil, APIManager.Invalid.format.localizedError)
-                return
-            }
+				return
+			}
 			
 			let bitcoin = self.currentBitcoinInformation(from: data)
 			completion?(bitcoin.bitcoin, nil)
@@ -64,21 +64,21 @@ extension BitcoinInformationManager {
 	
 	/// Method to decode json into bitcoin models
 	/// - Parameter data: data received from request
-    private static func bitcoinInformation(from data: Data) -> (bitcoinDayInformations: [BitcoinDayInformation], error: Error?) {
-        do {
-            let bitcoinData = try JSONDecoder().decode(BitcoinData.self, from: data)
-            return (bitcoinDayInformations: bitcoinData.bitcoinSubdata.bitcoinDayInformations, error: nil)
-        } catch let error {
-            return (bitcoinDayInformations: [], error: error)
-        }
-    }
+	private static func bitcoinInformation(from data: Data) -> (bitcoinDayInformations: [BitcoinDayInformation], error: Error?) {
+		do {
+			let bitcoinData = try JSONDecoder().decode(BitcoinData.self, from: data)
+			return (bitcoinDayInformations: bitcoinData.bitcoinSubdata.bitcoinDayInformations, error: nil)
+		} catch let error {
+			return (bitcoinDayInformations: [], error: error)
+		}
+	}
 	
 	private static func currentBitcoinInformation(from data: Data) -> (bitcoin: Bitcoin?, error: Error?) {
 		do {
-            let bitcoin = try JSONDecoder().decode(Bitcoin.self, from: data)
+			let bitcoin = try JSONDecoder().decode(Bitcoin.self, from: data)
 			return (bitcoin: bitcoin, error: nil)
-        } catch let error {
-            return (bitcoin: nil, error: error)
-        }
+		} catch let error {
+			return (bitcoin: nil, error: error)
+		}
 	}
 }
